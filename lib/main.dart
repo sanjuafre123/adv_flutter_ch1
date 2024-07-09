76f6ee29122d121e24d834a32f9700ff40d5c0d4
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-09 10:37:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'StepperDemo/Stepper.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,81 +16,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
     return MaterialApp(
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SizedBox(
-          height: height,
-          width: width,
-          child: Column(
-            children: [
-              SizedBox(
-                height: height * 0.2,
-              ),
-              const Text(
-                'Yo Man!',
-                style: TextStyle(
-                    fontSize: 42,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1),
-              ),
-              SizedBox(
-                height: height * 0.013,
-              ),
-              const Text(
-                'it`s a simple example of\n          dark theme',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 1,
-                    color: Colors.grey),
-              ),
-              SizedBox(
-                height: height * 0.08,
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    isDark = false;
-                  });
-                },
-                child:
-                    lightDarkButton(height, width, 'Light Mode', Colors.amber),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    isDark = true;
-                  });
-                },
-                child: lightDarkButton(height, width, 'Dark Mode', Colors.red),
-              ),
-            ],
-          ),
-        ),
-      ),
+      routes: {
+        '/' : (context)=> const StepperExampleApp(),
+      },
     );
   }
 }
-Container lightDarkButton(double height, double width, text, color) {
-  return Container(
-    margin: const EdgeInsets.fromLTRB(25, 25, 25, 5),
-    alignment: Alignment.center,
-    height: height * 0.1,
-    width: width,
-    decoration: BoxDecoration(
-      color: color,
-      borderRadius: BorderRadius.circular(10),
-    ),
-    child: Text(
-      text,
-      style: const TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-    ),
-  );
-}
-bool isDark = false;
