f1fbf1beda6fbfb5e1b44105b4cce77376081989
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-10 21:56:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'Screen/Lec-1.4/provide/theme_change_provider.dart';
 import 'Screen/Lec-1.4/view/change_theme.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -18,10 +17,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ChangeTheme(),
+    return ChangeNotifierProvider(
+      create: (context) => ThemeChangeProvider(),
+      builder: (context, child) => MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          colorScheme: const ColorScheme.light(
+            surface: Color(0xff9730a4),
+            onInverseSurface: Color(0xff5ea669),
+            onTertiary: Color(0xff3a91d6),
+            onTertiaryContainer: Color(0xffc56763),
+          ),
+
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          colorScheme: const ColorScheme.dark(
+            surface: Color(0xffc57b24),
+            onInverseSurface: Color(0xff4478ac),
+            onTertiary: Color(0xffd1275e),
+            onTertiaryContainer: Color(0xffe2de7f),
+          ),
+        ),
+        themeMode: Provider.of<ThemeChangeProvider>(context).isDark? ThemeMode.dark : ThemeMode.light,
+        debugShowCheckedModeBanner: false,
+        home: const ChangeTheme(),
+      ),
     );
   }
 }
-
