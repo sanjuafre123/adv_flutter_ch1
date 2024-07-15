5b4b784c1a5f3711accfcb65b0eb1c16124aebdf
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-15 09:52:46 +0530

    Update welcomeProvider.dart

--- lib/Screen/Lec-1.5.2/provider/welcomeProvider.dart
+++ lib/Screen/Lec-1.5.2/provider/welcomeProvider.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class IntroProvider extends ChangeNotifier{
+  bool isWelcome = false;
+  late SharedPreferences sharedPreferences;
 
+  void checkIsHome() {
+    isWelcome = true;
+    setPreferences(isWelcome);
+    notifyListeners();
+  }
 
+  Future<void> setPreferences(bool value) async {
+    sharedPreferences  = await SharedPreferences.getInstance();
+    sharedPreferences.setBool('home', value);
+  }
+
+  introScreenProvider(bool isWelcome){
+    isWelcome = isWelcome;
+    notifyListeners();
+  }
 }
