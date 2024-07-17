361785adba49b4005416157e5c382fa88bbf76a3
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-17 09:31:30 +0530

    Update Url_provider.dart

--- lib/Screen/Lec-1.6/Provider/Url_provider.dart
+++ lib/Screen/Lec-1.6/Provider/Url_provider.dart
@@ -1,5 +1,33 @@
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LauncherProvider extends ChangeNotifier{
+
+  void phoneLauncher()
+  {
+    Uri uri = Uri.parse('tel: +91 9173074442');
+    launchUrl(uri);
+  }
+  void mailLauncher()
+  {
+    Uri uri = Uri.parse('mailto: sanjuafre08@gmail.com');
+    launchUrl(uri);
+  }
+  void linkLauncher()
+  {
+    Uri uri = Uri.parse('https://www.linkedin.com/in/sanju-afre-a294432a9/');
+    launchUrl(uri,mode: LaunchMode.inAppWebView);
+  }
+  void instLauncher()
+  {
+    Uri uri = Uri.parse('https://github.com/sanjuafre123');
+    launchUrl(uri,mode: LaunchMode.inAppWebView);
+  }
+  void gitLauncher()
+  {
+    Uri uri = Uri.parse('https://github.com/sanjuafre123');
+    launchUrl(uri,mode: LaunchMode.inAppWebView);
+  }
 
 }
