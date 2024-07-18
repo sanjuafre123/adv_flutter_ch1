fb9ebec979cf03a262b25526bd4e452e7b581ef3
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-18 09:30:10 +0530

    Update gallery2.dart

---
+++ lib/Screen/Lec-1.7/view/gallery2.dart
@@ -0,0 +1,128 @@
+
+import 'package:add_flutter/Utils/ImageList.dart';
+import 'package:flutter/material.dart';
+
+class HideScreen extends StatelessWidget {
+  const HideScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Icon(
+          Icons.menu,
+          size: 28,
+          color: Colors.black,
+        ),
+        centerTitle: true,
+        title: Text(
+          'Recent',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500,color: Colors.black),
+        ),
+        actions: [
+          Icon(Icons.search,color: Colors.black,),
+          Icon(Icons.more_vert,color: Colors.black,),
+          SizedBox(width: width*0.012,)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 14, right: 14, top: 12),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Today',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: width * 0.046,color: Colors.black),
+                  ),
+                  Text(
+                    'Select',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue,
+                        fontSize: width * 0.04),
+                  ),
+                ],
+              ),
+              SizedBox(height: height*0.02,),
+              Wrap(
+                spacing: width*0.0221,
+                children: [
+                  ...List.generate(data1.length, (index) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: height*0.080,
+                          width: width*0.166,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(data1[index]),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),),
+                ],
+              ),
+              SizedBox(height: height*0.004,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Today',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: width * 0.046,color: Colors.black),
+                  ),
+                  Text(
+                    'Select',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue,
+                        fontSize: width * 0.04),
+                  ),
+                ],
+              ),
+              SizedBox(height: height*0.02,),
+              Wrap(
+                spacing: width*0.0221,
+                children: [
+                  ...List.generate(data2.length, (index) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: height*0.080,
+                          width: width*0.166,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(data2[index]),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
