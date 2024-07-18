1137ad7926aba64f051d0d057ebcabd577a58790
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-18 09:28:36 +0530

    Update gallery_provider.dart

--- lib/Screen/Lec-1.7/provider/gallery_provider.dart
+++ lib/Screen/Lec-1.7/provider/gallery_provider.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
-class GalleryProvider extends ChangeNotifier{
+class GalleryProvider extends ChangeNotifier {
+  final localAuth = LocalAuthentication();
+  bool didAuthenticate = false;
 
+  Future<void> authication() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await localAuth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      'device not supported';
+    }
+    try {
+      didAuthenticate = await localAuth.authenticate(
+        localizedReason: 'Please authenticate to access secure data',
+      );
+      didAuthenticate = didAuthenticate;
+      notifyListeners();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
 }
