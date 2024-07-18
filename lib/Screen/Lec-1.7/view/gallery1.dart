ffd46bfdd0cd23398734aa4ed86a59680b833b43
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-18 09:29:00 +0530

    Update gallery1.dart

--- lib/Screen/Lec-1.7/view/gallery1.dart
+++ lib/Screen/Lec-1.7/view/gallery1.dart
@@ -1,17 +1,29 @@
+import 'package:add_flutter/Screen/Lec-1.7/view/Hidescreen.dart';
+import 'package:add_flutter/Utils/ImageList.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/gallery_provider.dart';
 
 class Gallery1Screen extends StatelessWidget {
   const Gallery1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    GalleryProvider galleryProviderFalse = Provider.of(
+      context,
+      listen: false,
+    );
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: const Icon(
           Icons.menu,
-          size: 32,
+          size: 28,
+          color: Colors.black,
         ),
-        elevation: 1,
+        elevation: 0.5,
         scrolledUnderElevation: 0.1,
         backgroundColor: Colors.white,
         shadowColor: Colors.black,
@@ -19,44 +31,125 @@ class Gallery1Screen extends StatelessWidget {
         title: const Text(
           'Gallery',
           style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 27, letterSpacing: 0.7),
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
+            letterSpacing: 0.7,
+            color: Colors.black,
+          ),
         ),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  DropdownButton(
-                    items: [
-                      DropdownMenuItem(
-                        child: Text(
-                          'Albums',
-                          style: TextStyle(
-                              color: Colors.blue, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 10, top: 15),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    DropdownButton(
+                      items: const [
+                        DropdownMenuItem(
+                          child: Text(
+                            'Albums',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                    const SizedBox(
+                      width: 179,
+                    ),
+                    Icon(
+                      Icons.search_outlined,
+                      size: 28,
+                      color: Colors.grey.shade700,
+                    ),
+                    PopupMenuButton(
+                      itemBuilder: (context) => popUpMenuItems,
+                      iconColor: Colors.black,
+                      onSelected: (value) async {
+                        if (value == 1) {
+                          await galleryProviderFalse.authication();
+                          if (galleryProviderFalse.didAuthenticate) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HideScreen(),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12,right: 6),
+                  child: Column(
+                    children: [
+                      Wrap(
+                        spacing: width * 0.032,
+                        children: [
+                          ...List.generate(
+                            galleryImgList.length,
+                            (index) => Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: height * 0.1422,
+                                    width: width * 0.283,
+                                    decoration: BoxDecoration(
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(18),
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: AssetImage(
+                                            galleryImgList[index]['img']),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: height * 0.07,
+                                    width: width * 0.275,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          galleryImgList[index]['cat'],
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: width * 0.044),
+                                        ),
+                                        Text(
+                                          galleryImgList[index]['num'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: width * 0.034,
+                                              color: Colors.grey.shade600),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
-                    onChanged: (value) {},
                   ),
-                  SizedBox(
-                    width: 262,
-                  ),
-                  Icon(
-                    Icons.search_outlined,
-                    size: 28,
-                    color: Colors.grey.shade700,
-                  ),
-                  PopupMenuButton(
-                    itemBuilder: (context) => popUpMenuItems,
-                    onSelected: (value) {},
-                  )
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -65,18 +158,16 @@ class Gallery1Screen extends StatelessWidget {
 }
 
 List<PopupMenuEntry> popUpMenuItems = [
-  PopupMenuItem(
+  const PopupMenuItem(
     value: 0,
     child: Text(
       'setting',
-      style: TextStyle(fontSize: 20),
     ),
   ),
-  PopupMenuItem(
+  const PopupMenuItem(
     value: 1,
     child: Text(
       'Show Hidden Folder',
-      style: TextStyle(fontSize: 17),
     ),
   )
 ];
