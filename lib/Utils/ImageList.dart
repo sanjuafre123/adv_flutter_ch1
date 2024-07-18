dddaa0afcc1c3b568ab1aa1148862321f7db6139
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-18 09:30:44 +0530

    Add files via upload

---
+++ lib/Utils/ImageList.dart
@@ -0,0 +1,108 @@
+
+
+List galleryImgList = [
+  {
+    'img': 'assets/galleryimg/1.jpeg',
+    'cat': 'Camara',
+    'num': '120',
+  },
+  {
+    'img': 'assets/galleryimg/2.jpeg',
+    'cat': 'Family',
+    'num': '520',
+  },
+  {
+    'img': 'assets/galleryimg/3.jpeg',
+    'cat': 'Facebook',
+    'num': '154',
+  },
+  {
+    'img': 'assets/galleryimg/4.jpeg',
+    'cat': 'WhatsApp',
+    'num': '545',
+  },
+  {
+    'img': 'assets/galleryimg/5.jpeg',
+    'cat': 'Screenshot',
+    'num': '565',
+  },
+  {
+    'img': 'assets/galleryimg/6.jpeg',
+    'cat': 'Instagram',
+    'num': '111',
+  },
+  {
+    'img': 'assets/galleryimg/7.jpeg',
+    'cat': 'Collage',
+    'num': '015',
+  },
+  {
+    'img': 'assets/galleryimg/8.jpeg',
+    'cat': 'Flower',
+    'num': '56',
+  },
+  {
+    'img': 'assets/galleryimg/9.jpeg',
+    'cat': 'Animals',
+    'num': '016',
+  },
+  {
+    'img': 'assets/galleryimg/10.jpeg',
+    'cat': 'Collage',
+    'num': '025',
+  },
+  {
+    'img': 'assets/galleryimg/11.jpeg',
+    'cat': 'Flower',
+    'num': '405',
+  },
+  {
+    'img': 'assets/galleryimg/12.jpeg',
+    'cat': 'Animals',
+    'num': '012',
+  },
+];
+
+List data1 = [
+  'assets/data/13.jpeg',
+  'assets/data/14.jpeg',
+  'assets/data/15.jpeg',
+  'assets/data/16.jpeg',
+  'assets/data/17.jpeg',
+  'assets/data/18.jpeg',
+  'assets/data/19.jpeg',
+  'assets/data/20.jpeg',
+  'assets/data/21.jpeg',
+  'assets/data/22.jpeg',
+  'assets/data/23.jpeg',
+  'assets/data/24.jpeg',
+  'assets/data/25.jpeg',
+  'assets/data/26.jpeg',
+  'assets/data/27.jpeg',
+  'assets/data/28.jpeg',
+  'assets/data/29.jpeg',
+  'assets/data/30.jpeg',
+  'assets/data/31.jpeg',
+  'assets/data/32.jpeg',
+];
+List data2 = [
+
+  'assets/data/33.jpeg',
+  'assets/data/34.jpeg',
+  'assets/data/35.jpeg',
+  'assets/data/36.jpeg',
+  'assets/data/37.jpeg',
+  'assets/galleryimg/1.jpeg',
+  'assets/galleryimg/2.jpeg',
+  'assets/galleryimg/3.jpeg',
+  'assets/galleryimg/4.jpeg',
+  'assets/galleryimg/5.jpeg',
+  'assets/galleryimg/6.jpeg',
+  'assets/galleryimg/7.jpeg',
+  'assets/galleryimg/8.jpeg',
+  'assets/galleryimg/9.jpeg',
+  'assets/galleryimg/10.jpeg',
+  'assets/galleryimg/11.jpeg',
+  'assets/galleryimg/12.jpeg',
+
+];
